f900ef0099590b04ffcfee93cfebe6884f60dae5
Ricardo Porras <54407546+JoseRicardoPorrasBarrantes@users.noreply.github.com>
2022-06-03 18:28:14 -0600

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+{
+    // Jose Ricardo Porras Barrantes    
+    // B96028
+    class Main
+    {
+        public String Mensaje()
+        {
+            String mensaje = "Hola Mundo";
+            return mensaje;
+        }
+    }
+}
